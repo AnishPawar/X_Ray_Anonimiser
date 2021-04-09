72c90af0b21f66a983297186cdd68d4a3da15e78
Anish Pawar <anish.pawar@somaiya.edu>
2021-04-10 03:45:32 +0530

    Trying Out Stuff

---
+++ Omega_Testing/boom.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np 
+# cap = cv2.VideoCapture(0)
+
+def mouse(event,x,y,z,w):
+    print(x,y)
+    pixel = frame[y,x]
+    print(pixel)
+    
+
+#Mouse function
+cv2.namedWindow('frame')
+cv2.setMouseCallback('frame',mouse)
+
+   
+while True:
+    frame = cv2.imread("credimages/visa.png")
+    # ret, frame = cap.read()     
+    
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# cap.release()
